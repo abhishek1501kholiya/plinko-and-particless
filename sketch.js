06a1b41a5ed69d4eab6773517cb7cf6863a50f5e
abhishek1501kholiya <abhishekkholiya15012007@gmai.com>
2020-06-18 05:01:57 +0530

    new hope

--- sketch.js
+++ sketch.js
@@ -3,82 +3,64 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var ground; 
+
+var particles = [] ;
+var plinkos  = [];
+var divisions   = [];
+
+var divisionHeight = 300;
+
 
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(450,800);
     engine = Engine.create();
     world = engine.world;
-
-
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
-}
-
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-}
-
-
-function mouseReleased(){
-    slingshot.fly();
-}
-
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
+    for(var k = 0 ; k <=width ; k =k +80){
+        divisions.push(new Divisions(k, height-divisionHeight/2 , 10, divisionHeight));
     }
+
+    for(var j = 40; j<=width; j=j+50){
+        plinkos.push(new Plinko(j,75));
+    }
+for(var j = 15 ; j<=width-10 ; j=j+50)
+plinkos.push(new Plinko(j,175));
+  
+}
+
+
+   
+function draw(){
+    background(0);
+    if(frameCount%60 ===0){
+        particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+    
+    }
+ 
+    ground.display();
+    for(var k = 0 ; k < divisions.length; k++ ){
+        divisions[k].display();
+    }
+    for(var  i= 0 ; i < plinkos.length; i++ ){
+        plinkos[i].display();
+    }
+    for(var  p= 0 ; p < plinkos.length ; p++ ){
+        plinkos[p].display();
+    }
+    for(var  j= 0 ; j < particles.length ; j++ ){
+        particles[j].display();
+    }
+
+
+   
+    //divisions.display();
+    Engine.update(engine);
+    
 }
